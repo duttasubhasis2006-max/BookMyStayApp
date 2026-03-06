1e4e8a4c3002c15584dd5d2c1ab942056537a73c
duttasubhasis2006-max <duttasubhasis2006@gmail.com>
2026-03-06 15:23:02 +0530

    Subhasis ADDED  UC1-WELCOME

---
+++ APP/src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking System v1.0");
+    }
+
+}
